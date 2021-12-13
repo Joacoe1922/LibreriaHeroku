dcdb5787f30a34525888ccd53844694abd29d522
Joaquín Coelho <coelhojoaquin@gmail.com>
2021-12-13 20:50:07 -0300

    2nd commit

--- src/main/java/com/egg/libreria/controladores/PortalControlador.java
+++ src/main/java/com/egg/libreria/controladores/PortalControlador.java
@@ -7,13 +7,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/")
+@CrossOrigin
 public class PortalControlador {
 
     @Autowired
